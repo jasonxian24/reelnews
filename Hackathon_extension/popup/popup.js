2b1bd375548900ce6b145246162f27409f4380e2
Sadhana Marikunte <t-samari@microsoft.com>
2020-06-27 22:01:34 -0400

    update but doesn't work

--- Hackathon_extension/popup/popup.js
+++ Hackathon_extension/popup/popup.js
@@ -1,33 +1,33 @@
 const analyze = document.getElementById("analyze");
-const url = 'reelapp.azurewebsites.net/article';
+const url = 'https://reelnewsserver.azurewebsites.net/article';
 var info = {
-    title: "",
-    publisher: ""
+    title : ""
 }
 
 if (analyze) {
   analyze.onclick = function() {
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
         document.write(`<h3>Current Tab Info: <h3>`);
-        document.write('<ul>');
+        document.write(`<ul>`);
         var url = "" + tabs[0].url;
         var url_parts = url.split('.');
         var publisher = url_parts[1];
-        var title = tabs[0].title;
+        var title = "" + tabs[0].title;
         info.title = title;
-        info.publisher = publisher;
-        document.write(`<li>${info.publisher}</li>`);
+        // info.publisher = publisher;
+        // document.write(`<li>${info.publisher}</li>`);
         document.write(`<li>${info.title}</li>`);
         
-        fetch(url, {body: info, method: "POST"}).then(res => {
-            return res.json();
-        }, err => {
-            document.write(`<li>${err}</li>`)
+        fetch(url, {body: {title : "Microsoft"}, method: "POST"
+        }).then(res => {
+            return res
         }).then(data => {
-            document.write(`<li>${data}</li>`)
+            document.write(`<li>${data[0]}</li>`)
+        }).catch(error=>{
+            document.write(`<li>${error}</li>`)
         });  
 
-        document.write('<ul>');
+        document.write(`<ul>`);
 
     })
 }
