7f6110395e4a2f8dd747ce282437ad3a27322016
Dhruv Iyer <t-dhiyer@microsoft.com>
2020-06-27 23:29:52 -0700

    nice ui

--- Hackathon_extension/popup/popup.js
+++ Hackathon_extension/popup/popup.js
@@ -4,11 +4,11 @@ var info = {
     title : ""
 }
 
-if (analyze) {
-  analyze.onclick = function() {
+
+//if (analyze) {
+  //analyze.onclick = function() {
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-        document.write(`<h3>Recommended Articles: </h3>`);
-        document.write(`<ul>`)
+        
         var url = "" + tabs[0].url;
         var url_parts = url.split('.');
         var publisher = url_parts[1];
@@ -24,27 +24,50 @@ if (analyze) {
             method: "POST" 
         }
         
+        
+        
         fetch('https://reelnewsserver.azurewebsites.net/article', params).then(res => {
             return res.json()
         }).then(data => {
+            document.write(`<h1>The Other Side </h1>`);
+            document.write(`<p><em>Articles from across the aisle, powered by <img src="/icon/bing.png"> </em></p><br><br>`);
+            document.write(`<ul style="list-style:none;list-style-position: inside; padding-left:0;">`);
+        
             for(var i = 0; i < data.length; i++) {
-                var str = "" + data[i][0] + '-' + data[i][2]
+                var str = "" + data[i][0]
+                str = str.substring(0, Math.min(str.length, 30 )) + "..."
                 var link = str.link(data[i][1])
-                document.write(`<li>${link}</li>`) 
+                document.write(`<li style=" margin-bottom: 10px;"><a href="${data[i][1]}" title="${data[i][0]}">${str}</a> | ${data[i][2]} </li>`) 
                 
 
             }
+            document.write(`</ul>`);
+            document.body.style.zIndex = 1;
+            document.body.style.width = "250px";
+            document.body.style.background = "#E6E6E6";
+            document.body.style.fontFamily = "Segoe UI";
+
+            var links = document.getElementsByTagName("a");
+            for (var i = 0; i < links.length; i++) {
+                (function () {
+                    var ln = links[i];
+                    var location = ln.href;
+                    console.log(location);
+                    ln.onclick = function () {
+                        chrome.tabs.create({active: true, url: location});
+                    };
+                })();
+            }
+
+            
             
         }).catch(error=>{
             document.write(`<li>${error}</li>`)
         });  
 
-
-        document.write(`<ul>`)
-
         
-
     })
-}
-}
+
+//}
+//}
 
